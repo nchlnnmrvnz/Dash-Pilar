951be5c480f93bcf26239f5c43ad668086e3f9e4
nchlnnmrvnz <avanez.nichole@gmail.com>
2023-10-31 17:00:46 +0800

    feat: ADD MainActivity.java functionality

--- app/src/main/java/com/example/dashpilar/MainActivity.java
+++ app/src/main/java/com/example/dashpilar/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.dashpilar;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    Button btnDineIn, btnTakeOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnDineIn = findViewById(R.id.btnDineIn);
+        btnTakeOut = findViewById(R.id.btnTakeOut);
+
+        btnDineIn.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, MainOrderMenu.class)));
+
+        btnTakeOut.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, MainOrderMenu.class)));
     }
 }
---
+++ app/src/main/java/com/example/dashpilar/MainOrderMenu.java
@@ -0,0 +1,14 @@
+package com.example.dashpilar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainOrderMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_order_menu);
+    }
+}
