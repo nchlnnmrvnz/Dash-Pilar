f7e62fdaf0cc476b8dc8763371fd26360e7c6c64
nchlnnmrvnz <avanez.nichole@gmail.com>
2023-12-11 15:06:52 +0800

    feat: ADD classes for printing

---
+++ app/src/main/java/com/example/dashpilar/USBConnection.java
@@ -0,0 +1,13 @@
+package com.example.dashpilar;
+
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.IntentFilter;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbManager;
+
+public class USBConnection {
+
+    }
+}
